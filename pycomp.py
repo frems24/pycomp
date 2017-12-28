459ded5a005bed1e07a6ca2201cb4eb399d6af1a
Wojciech G <wg@pwr.edu.pl>
2017-12-28 15:12:36 +0100

    Po utworzeniu nagłówka z wyrazami do docelowego pliku.

--- pycomp.py
+++ pycomp.py
@@ -1,6 +1,5 @@
 # pycomp.py -- kompresuje pliki tekstowe metodą słownikową
 # wersja 0.1
-# tylko ASCII ?
 
 import sys
 from operator import itemgetter
@@ -17,24 +16,42 @@ def main():
         for line in input_file:
             update_words_dict(line.rstrip(), words_dict, 2, 3)
 
-    # words_list = list(words_dict.items())
-    # words_len_3 = sorted([wl3 for wl3 in words_list if wl3[1][0] == 3], key=itemgetter(1), reverse=True)
-    # words_len_4 = sorted([wl4 for wl4 in words_list if wl4[1][0] == 4], key=itemgetter(1), reverse=True)
-    # words_len_more_4 = sorted([wlm4 for wlm4 in words_list if wlm4[1][0] > 4], key=itemgetter(1))
-
-    # Utworzenie posortowanej tablicy słów dla nagłówka
+    # Utworzenie posortowanej listy słów
     words_sorted_list = sorted(words_dict.items(), key=itemgetter(1), reverse=True)
-    for n, (word, stats) in enumerate(words_sorted_list):
-        print("%3d%13s\t%d\t%d" % (n, word, stats[0], stats[1]))
+    words_total = len(words_sorted_list)
 
-    # for n, (word, stats) in enumerate(words_len_3):
-    #     print("%d%13s\t%d\t%d" % (n, word, stats[0], stats[1]))
-    # print()
-    # for n, (word, stats) in enumerate(words_len_4):
-    #     print("%d%13s\t%d\t%d" % (n, word, stats[0], stats[1]))
-    # print()
-    # for n, (word, stats) in enumerate(words_len_more_4):
-    #     print("%d%13s\t%d\t%d" % (n, word, stats[0], stats[1]))
+    # Utworzenie listy słów dla nagłówka
+    dst_words_index = 0
+    src_words_index = 0
+
+    words_0_to_9 = []
+    while dst_words_index < 10 and src_words_index < words_total:
+        if words_sorted_list[src_words_index][1][1] > 2:
+            words_0_to_9.append(words_sorted_list[src_words_index][0])
+            dst_words_index += 1
+        src_words_index += 1
+
+    words_10_to_99 = []
+    while dst_words_index < 100 and src_words_index < words_total:
+        if words_sorted_list[src_words_index][1][1] > 3:
+            words_10_to_99.append(words_sorted_list[src_words_index][0])
+            dst_words_index += 1
+        src_words_index += 1
+
+    words_100_to_999 = []
+    while dst_words_index < 1000 and src_words_index < words_total:
+        if words_sorted_list[src_words_index][1][1] > 4:
+            words_100_to_999.append(words_sorted_list[src_words_index][0])
+            dst_words_index += 1
+        src_words_index += 1
+
+    header_list = words_0_to_9 + words_10_to_99 + words_100_to_999
+    for n, word in enumerate(header_list):
+        print("%s " % word, end='')
+
+    print("\ntotal: %d" % words_total)
+    print("dst: %d" % dst_words_index)
+    print("src: %d" % src_words_index)
 
     # Zapisanie nagłówka w docelowym pliku
     # Kompresja pliku
